d1b5c10ed1b5790148c77ce1bebd4d02e6da5f59
rali1 <webcoder1@gmail.com>
2015-11-10 13:33:34 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var xslt4node = require('xslt4node');
+var path = require('path');
+
+
+//transform route
+router.post('/', function(req, res) {
+    var config = {
+        xsltPath: 'bwcktrans.xslt',
+        source: req.body.doc,
+        result: 'result.html'
+    };
+
+    xslt4node.transform(config, function (err) {
+        if (err) {
+            console.log(err);
+            res.send('error');
+        } else {
+            res.sendFile(path.dirname(module.parent.filename) + '/result.html');
+        }
+    });
+});
+
+module.exports = router;
