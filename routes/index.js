54445dc5973fdfbdab924ff879ee9e151612e422
Todd Hiles <todd.hiles@georgiancollege.ca>
2015-12-17 17:28:33 -0500

    Allow app to be served at a non-root path. - in our case IIS is configured to serve this app at path /transformdoc within the apps.georgiantest.com site. - removed superfluous users route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var xslt4node = require('xslt4node');
 var path = require('path');
 
-
 //transform route
-router.post('/', function(req, res) {
+router.post(process.env.ROOTPATH || '/', transformDoc);
+
+function transformDoc(req, res) {
     var config = {
         xsltPath: 'bwcktrans.xslt',
         source: req.body.doc,
@@ -15,13 +16,14 @@ router.post('/', function(req, res) {
     xslt4node.transform(config, function (err) {
         if (err) {
             console.log(err);
-            res.send('error');
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+            res.status(500).send({ error: 'Something blew up!' });
         } else {
             res.header('Access-Control-Allow-Origin', '*');
-			res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+            res.header('Access-Control-Allow-Headers', 'X-Requested-With');
             res.sendFile(path.dirname(module.parent.filename) + '/result.html');
         }
     });
-});
-
+}
 module.exports = router;
